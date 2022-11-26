149837c9a25da26d74c6b14c1ed04db83878e70b
Hubert Pan <hubert.pan3@gmail.com>
2022-11-26 15:51:28 -0500

    checkpoint: finish first draft of ode model generation, fix typos in analysis

--- scripts/model_221/model_221.py
+++ scripts/model_221/model_221.py
@@ -18,6 +18,8 @@ class PVH_Model_211:
     vector_growth_rates: dict,
     vector_mortality_rates: dict,
     vector_population_sizes: dict,
+    vector_transStadialOvarial: dict,
+    vector_cofeeding: dict,
     max_vectors_per_host: dict):
         """Initialize the PVH_Model and verify that the inputs are valid"""
         self.pathogens = pathogens
@@ -39,6 +41,8 @@ class PVH_Model_211:
         self.vector_mortality_rates = vector_mortality_rates
         self.vector_population_sizes = vector_population_sizes
         self.max_vectors_per_hosts = max_vectors_per_host
+        self.vector_transStadialOvarial = vector_transStadialOvarial
+        self.vector_cofeeding = vector_cofeeding
         # validate inputs
         self.validate()
         # generate infection states
@@ -51,6 +55,12 @@ class PVH_Model_211:
         each time the ode step is run the following input vector will be provided
         [N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12]
         """
+        def dNdt(input):
+            N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
+            beta = self.host_growth_rates["Y"]
+            K = self.host_carrying_capacities["Y"]
+            b = self.host_mortality_rates["Y"]
+            return  beta * ((K - N)/ (K)) * N - b*N
         def dY1dt(input):
             N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
             # constants
@@ -71,7 +81,8 @@ class PVH_Model_211:
             coinfW = - A12W * (Y1/N) * (Z2 + Z12)
             growth = - Y_growth * ((N*Y1)/(K))
             deathAndRecovery = - (b + v1_0) * Y1
-            return sum(X1_toY, W1_toY, recov_from_coinf, coinfX, coinfW, growth, deathAndRecovery)
+            return sum([X1_toY, W1_toY, recov_from_coinf, coinfX, coinfW, 
+                        growth, deathAndRecovery])
         def dY2dt(input):
             N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
             # constants
@@ -92,7 +103,8 @@ class PVH_Model_211:
             coinfW = - A12W * (Y1/N) * (Z1 + Z12)
             growth = - Y_growth * ((N*Y2)/(K))
             deathAndRecovery = - (b + v2_0) * Y2
-            return sum(X2_toY, W2_toY, recov_from_coinf, coinfX, coinfW, growth, deathAndRecovery)
+            return sum([X2_toY, W2_toY, recov_from_coinf, coinfX, coinfW, 
+                        growth, deathAndRecovery])
         def dY12dt(input):
             N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
             # constants
@@ -113,28 +125,187 @@ class PVH_Model_211:
             W1_toY = A2W * ((Y2)/(N)) (Z2 + Z12)
             growth = - Y_growth * ((N*Y12)/(K))
             deathAndRecovery = - (b + v12_1 + v12_2) * Y12
-            return sum(X2_toY, W2_toY, X1_toY, W1_toY, recov, coinfX, coinfW, growth, deathAndRecovery)
+            return sum([X2_toY, W2_toY, X1_toY, W1_toY, growth, deathAndRecovery])
+        def dVdt(input):
+            N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
+            betaHat = self.vector_growth_rates["V"]
+            M = self.max_vectors_per_hosts["V"]
+            b_hat = self.vector_mortality_rates["V"]
+            return  betaHat * ((M*N - V)/(M*N)) * V - b_hat * V
         def dX1dt(input):
             N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
             # constants
-            
+            AHat1V = self.host_to_vector_trx_consts["Y"][""]["1"]["V"]
+            betaHatV = self.vector_growth_rates["V"]
+            gamma1 = self.vector_transStadialOvarial["V"]["1"]
+            gamma12 = self.vector_transStadialOvarial["V"]["12"]
+            mu_1V = self.vector_cofeeding["V"]["V"][""]["1"]
+            mu_1W = self.vector_cofeeding["W"]["V"][""]["1"]
+            AHat1to12V = self.host_to_vector_trx_consts["Y"]["1"]["2"]["V"]
+            mu_2to12V = self.vector_cofeeding["V"]["V"]["1"]["2"]
+            mu_2to12W = self.vector_cofeeding["W"]["V"]["1"]["2"]
+            v_growth = self.vector_growth_rates["V"]
+            M_V = self.max_vectors_per_hosts["V"]["Y"]
+            bHat = self.vector_mortality_rates["V"]
             # components
-            host2TickInf = 0
-            trans = 0
-            cofeedingXInc = 0
-            cofeedingWInc = 0
-            host2TickCoinf = 0
-            cofeedingXDec = 0
-            cofeedingWDec = 0
-            growthDec = 0
-            death = 0
-            return sum()
+            host2TickInf = AHat1V * ((Y1 + Y12)/(N)) * (V - X1 - X2 - X12)
+            trans = betaHatV * (gamma1 * X1 + gamma12 * X12)
+            cofeedingXInc = mu_1V * ((X1 + X12)/(V)) * (V - X1 - X2 - X12)
+            cofeedingWInc = mu_1W * ((Z1 + Z12)/(W)) * (V - X1 - X2 - X12)
+            host2TickCoinf = - AHat1to12V * ((Y2 + Y12)/(N)) * X1 
+            cofeedingXDec = - mu_2to12V * ((X2 + X12)/(V)) * X1
+            cofeedingWDec = - mu_2to12W * ((Z2 + Z12)/(W)) * X1 
+            growthDec = v_growth * ((V * X1)/(M_V * N))
+            death = -bHat * X1 
+            return sum([host2TickInf, trans, cofeedingXInc, cofeedingWInc, 
+                host2TickCoinf, cofeedingXDec, cofeedingWDec, growthDec, death])
+        def dX2dt(input):
+            N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
+            # constants
+            AHat2V = self.host_to_vector_trx_consts["Y"][""]["2"]["V"]
+            betaHatV = self.vector_growth_rates["V"]
+            gamma2 = self.vector_transStadialOvarial["V"]["2"]
+            gamma12 = self.vector_transStadialOvarial["V"]["12"]
+            mu_2V = self.vector_cofeeding["V"]["V"][""]["2"]
+            mu_2W = self.vector_cofeeding["W"]["V"][""]["2"]
+            AHat2to12V = self.host_to_vector_trx_consts["Y"]["2"]["1"]["V"]
+            mu_2to12V = self.vector_cofeeding["V"]["V"]["2"]["1"]
+            mu_2to12W = self.vector_cofeeding["W"]["V"]["2"]["1"]
+            v_growth = self.vector_growth_rates["V"]
+            M_V = self.max_vectors_per_hosts["V"]["Y"]
+            bHat = self.vector_mortality_rates["V"]
+            # components
+            host2TickInf = AHat2V * ((Y2 + Y12)/(N)) * (V - X1 - X2 - X12)
+            trans = betaHatV * (gamma2 * X2 + gamma12 * X12)
+            cofeedingXInc = mu_2V * ((X2 + X12)/(V)) * (V - X1 - X2 - X12)
+            cofeedingWInc = mu_2W * ((Z2 + Z12)/(W)) * (V - X1 - X2 - X12)
+            host2TickCoinf = - AHat2to12V * ((Y2 + Y12)/(N)) * X2  
+            cofeedingXDec = - mu_2to12V * ((X2 + X12)/(V)) * X2
+            cofeedingWDec = - mu_2to12W * ((Z2 + Z12)/(W)) * X2 
+            growthDec = v_growth * ((V * X2)/(M_V * N))
+            death = -bHat * X2 
+            return sum([host2TickInf, trans, cofeedingXInc, cofeedingWInc, 
+                host2TickCoinf, cofeedingXDec, cofeedingWDec, growthDec, death])
+        def dX12dt(input):
+            N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
+            # constants
+            AHat1to12V = self.host_to_vector_trx_consts["Y"]["1"]["2"]["V"]
+            AHat2to12V = self.host_to_vector_trx_consts["Y"]["2"]["1"]["V"]
+            mu_1to12V = self.vector_cofeeding["V"]["V"]["1"]["2"]
+            mu_2to12V = self.vector_cofeeding["V"]["V"]["2"]["1"]
+            mu_1to12W = self.vector_cofeeding["W"]["V"]["1"]["2"]
+            mu_2to12W = self.vector_cofeeding["W"]["V"]["2"]["1"]
+            v_growth = self.vector_growth_rates["V"]
+            M_V = self.max_vectors_per_hosts["V"]["Y"]
+            bHat = self.vector_mortality_rates["V"]
+            # components
+            host2TickInf1 = AHat1to12V * ((Y2 + Y12)/(N)) * X1
+            host2TickInf2 = AHat2to12V * ((Y1 + Y12)/(N)) * X2
+            cofeeding1to12X = mu_1to12V * ((X2 + X12)/V) * X1 
+            cofeeding2to12X = mu_2to12V * ((X1 + X12)/V) * X2
+            cofeeding1to12Z = mu_1to12W * ((Z2 + Z12)/W) * X1 
+            cofeeding2to12Z = mu_2to12W * ((Z1 + Z12)/W) * X2
+            growthDec = - v_growth * ((V * X12)/(M_V/N))
+            death = bHat * X12 
+            return sum([host2TickInf1, host2TickInf2,
+                     cofeeding1to12X, cofeeding2to12X, cofeeding1to12Z, cofeeding2to12Z,
+                     growthDec, death])
+        def dWdt(input):
+            N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
+            betaHat = self.vector_growth_rates["W"]
+            M = self.max_vectors_per_hosts["W"]
+            b_hat = self.vector_mortality_rates["W"]
+            return  betaHat * ((M*N - W)/(M*N)) * W - b_hat * W
+        def dZ1dt(input):
+            N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
+            # constants
+            AHat1W = self.host_to_vector_trx_consts["Y"][""]["1"]["W"]
+            betaHatW = self.vector_growth_rates["W"]
+            gamma1 = self.vector_transStadialOvarial["W"]["1"]
+            gamma12 = self.vector_transStadialOvarial["W"]["12"]
+            mu_1V = self.vector_cofeeding["V"]["W"][""]["1"]
+            mu_1W = self.vector_cofeeding["W"]["W"][""]["1"]
+            AHat1to12W = self.host_to_vector_trx_consts["Y"]["1"]["2"]["W"]
+            mu_2to12V = self.vector_cofeeding["V"]["W"]["1"]["2"]
+            mu_2to12W = self.vector_cofeeding["W"]["W"]["1"]["2"]
+            W_growth = self.vector_growth_rates["W"]
+            M_W = self.max_vectors_per_hosts["W"]["Y"]
+            bHat = self.vector_mortality_rates["W"]
+            # components
+            host2TickInf = AHat1W * ((Y1 + Y12)/(N)) * (W - Z1 - Z2 - Z12)
+            trans = betaHatW * (gamma1 * Z1 + gamma12 * Z12)
+            cofeedingXInc = mu_1V * ((X1 + X12)/(V)) * (W - Z1 - Z2 - Z12)
+            cofeedingWInc = mu_1W * ((Z1 + Z12)/(W)) * (W - Z1 - Z2 - Z12)
+            host2TickCoinf = - AHat1to12W * ((Y2 + Y12)/(N)) * Z1 
+            cofeedingXDec = - mu_2to12V * ((X2 + X12)/(V)) * Z1
+            cofeedingWDec = - mu_2to12W * ((Z2 + Z12)/(W)) * Z1 
+            growthDec = W_growth * ((W * Z1)/(M_W * N))
+            death = -bHat * Z1 
+            return sum([host2TickInf, trans, cofeedingXInc, cofeedingWInc, 
+                host2TickCoinf, cofeedingXDec, cofeedingWDec, growthDec, death])
+        def dZ2dt(input):
+            N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
+            # constants
+            AHat2W = self.host_to_vector_trx_consts["Y"][""]["2"]["W"]
+            betaHatW = self.vector_growth_rates["W"]
+            gamma2 = self.vector_transStadialOvarial["W"]["2"]
+            gamma12 = self.vector_transStadialOvarial["W"]["12"]
+            mu_2V = self.vector_cofeeding["V"]["W"][""]["2"]
+            mu_2W = self.vector_cofeeding["W"]["W"][""]["2"]
+            AHat2to12W = self.host_to_vector_trx_consts["Y"]["2"]["1"]["W"]
+            mu_2to12V = self.vector_cofeeding["V"]["W"]["2"]["1"]
+            mu_2to12W = self.vector_cofeeding["W"]["W"]["2"]["1"]
+            w_growth = self.vector_growth_rates["W"]
+            M_W = self.max_vectors_per_hosts["W"]["Y"]
+            bHat = self.vector_mortality_rates["W"]
+            # components
+            host2TickInf = AHat2W * ((Y2 + Y12)/(N)) * (W - Z1 - Z2 - Z12)
+            trans = betaHatW * (gamma2 * Z2 + gamma12 * Z12)
+            cofeedingXInc = mu_2V * ((X2 + X12)/(V)) * (W - Z1 - Z2 - Z12)
+            cofeedingWInc = mu_2W * ((Z2 + Z12)/(W)) * (W - Z1 - Z2 - Z12)
+            host2TickCoinf = - AHat2to12W * ((Y1 + Y12)/(N)) * Z2 
+            cofeedingXDec = - mu_2to12V * ((X1 + X12)/(V)) * Z2 
+            cofeedingWDec = - mu_2to12W * ((Z1 + Z12)/(W)) * Z2  
+            growthDec = w_growth * ((V * X2)/(M_W * N))
+            death = -bHat * X2 
+            return sum([host2TickInf, trans, cofeedingXInc, cofeedingWInc, 
+                host2TickCoinf, cofeedingXDec, cofeedingWDec, growthDec, death])
+        def dZ12dt(input):
+            N, Y1, Y2, Y12, V, X1, X2, X12, W, Z1, Z2, Z12 = input
+            # constants
+            AHat1to12W = self.host_to_vector_trx_consts["Y"]["1"]["2"]["W"]
+            AHat2to12 = self.host_to_vector_trx_consts["Y"]["2"]["1"]["W"]
+            mu_1to12V = self.vector_cofeeding["V"]["W"]["1"]["2"]
+            mu_2to12V = self.vector_cofeeding["V"]["W"]["2"]["1"]
+            mu_1to12W = self.vector_cofeeding["W"]["W"]["1"]["2"]
+            mu_2to12W = self.vector_cofeeding["W"]["W"]["2"]["1"]
+            growth = self.vector_growth_rates["W"]
+            M = self.max_vectors_per_hosts["W"]["Y"]
+            bHat = self.vector_mortality_rates["W"]
+            # components
+            host2TickInf1 = AHat1to12W * ((Y2 + Y12)/(N)) * Z1
+            host2TickInf2 = AHat2to12 * ((Y1 + Y12)/(N)) * Z2
+            cofeeding1to12X = mu_1to12V * ((X2 + X12)/V) * Z1 
+            cofeeding2to12X = mu_2to12V * ((X1 + X12)/V) * Z2
+            cofeeding1to12Z = mu_1to12W * ((Z2 + Z12)/W) * Z1 
+            cofeeding2to12Z = mu_2to12W * ((Z1 + Z12)/W) * Z2
+            growthDec = - growth * ((W * Z12)/(M/N))
+            death = bHat * Z12 
+            return sum([host2TickInf1, host2TickInf2,
+                     cofeeding1to12X, cofeeding2to12X, cofeeding1to12Z, cofeeding2to12Z,
+                     growthDec, death])
         def model_ode(times, init: List[float], params: List[float]) -> List[float]:
             """
             This function represents the model
             times and params are completely ignored as each step is time dependent and the params are also assumed to be constant over time
             """
-            eqns = [dY1dt, dY2dt, dY12dt]
+            eqns = [dNdt, 
+                    dY1dt, dY2dt, dY12dt, 
+                    dVdt,
+                    dX1dt, dX2dt, dX12dt,
+                    dWdt,
+                    dZ1dt, dZ2dt, dZ12dt
+                    ]
             return list(map(lambda f: f(init), eqns))
         return model_ode
         
