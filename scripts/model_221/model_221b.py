ae5be05c97cab3d53ec5f42db2a106b5870f507f
Hubert Pan <hubert.pan3@gmail.com>
2022-11-30 06:18:44 -0500

    checkpoint

--- scripts/model_221/model_221b.py
+++ scripts/model_221/model_221b.py
@@ -15,14 +15,29 @@ def gaussin_probability(value):
     #np.random.seed(np.random.choice(range(1000), size=1))
     return np.random.normal(loc=value, scale=(value*4), size=None)
 
-def model_212(initial_states, hosts_parameters, vector_parameters, natural_parameters):
-    '''single iteration of all states in 212 model'''
+def model_221(times, initial_states, concatenated_params):
+    '''single iteration of all states in 221 model'''
+    hosts_parameters = concatenated_params[0:8]
+    vector_parameters = concatenated_params[8:28]
+    natural_parameters = concatenated_params[28:]
 
-    hosts_parameters = [gaussin_probability(x) for x in hosts_parameters]
-    vector_parameters = [gaussin_probability(x) for x in vector_parameters]
+    # hosts_parameters = [gaussin_probability(x) for x in hosts_parameters]
+    # vector_parameters = [gaussin_probability(x) for x in vector_parameters]
 
     '''stating variables'''
     N, V, W, Y1, Y2, Y12, X1, X2, X12, Z1, Z2, Z12 = initial_states
+    # N = max(N, 0)
+    # V = max(V, 0)
+    # W = max(W,0)
+    # Y1 = max(Y1, 0)
+    # Y2 = max(Y2, 0)
+    # Y12 = max(Y12, 0)
+    # X1 = max(X1, 0)
+    # X2 = max(X2, 0)
+    # X12 = max(X12, 0)
+    # Z1 = max(Z1, 0)
+    # Z2 = max(Z2, 0)
+    # Z12 = max(Z12, 0)
     A1, A2, A12, A21, r1, r2, r12, r21 = hosts_parameters
     Ahat1, Ahat2, Ahat12, Ahat21, gv1, gv2, gv12, gv21, gw1, gw2, gw12, gw21, uv1, uv2, uv12, uv21, uw1, uw2, uw12, uw21 = vector_parameters
     B, b, Bv, bv, Bw, bw, K, Mv, Mw = natural_parameters
@@ -130,50 +145,12 @@ def model_212(initial_states, hosts_parameters, vector_parameters, natural_param
     dZ12 = ( 0 
     + (Ahat12 * ((Y2 + Y12) / N) * Z1) # host to tick 1 -> 12 coinfection
     + (Ahat21 * ((Y1 + Y12) / N) * Z2) # host to tick 2 -> 12 coinfection
-    + (uw12 * ((X2 + X12) * Z1) / V) # cofeeding with V, 1 to 12 coinfection
+    + (uv12 * ((X2 + X12) * Z1) / V) # cofeeding with V, 1 to 12 coinfection
     + (uv21 * ((X1 + X12) * Z2) / V) # cofeeding with V, 2 to 12 coinfection
     + (uw12 * ((Z2 + Z12) * Z1) / W) # cofeeding with W, 1 to 12 coinfection
     + (uw21 * ((Z1 + Z12) * Z2) / W) # cofeeding with W, 2 to 12 coinfection
     - ((Bw * W * Z12) / (Mw * N)) # decrement due to carrying capacity 
     - (bw * Z12)) # decrement due to death
-
+    # print([N, V, W, Y1, Y2, Y12, X1, X2, X12, Z1, Z2, Z12])
+    # print([dN, dV, dW, dY1, dY2, dY12, dX1, dX2, dX12, dZ1, dZ2, dZ12])
     return [dN, dV, dW, dY1, dY2, dY12, dX1, dX2, dX12, dZ1, dZ2, dZ12]
-
-def main():
-
-    times = np.linspace(start=0, stop=10000, num=10000, endpoint=True, retstep=False)
-
-    initial_states = [100, 4000, 1, 1, 0, 1, 1, 0, 1, 1, 0]
-    '''N, V, W, Y1, Y2, Y12, X1, X2, X12, Z1, Z2, Z12'''
-    hosts_parameters = [0.4, 0.4, 0.2, 0.2, 0.1, 0.1, 0.05, 0.05]
-    '''A1, A2, A12, A21, r1, r2, r12, r21'''
-    vector_parameters = [0.4, 0.4, 0.2, 0.2, 0.1, 0.1, 0.05, 0.05, 0.1, 0.1, 0.05, 0.05]
-    '''Ahat1, Ahat2, Ahat12, Ahat21, g1, g2, g12, g21, u1, u2, u12, u21'''
-    natural_parameters = [0.01, 0.0001, 0.01, 0.0001, 0.01, 0.01, 10, 10, 20]
-    '''Ba, ba, Bb, bb, Bv, bv, Ka, Kb, M'''
-
-    SIR_solution = solve_ivp(lambda t, y: model_212(initial_states,
-                                                    hosts_parameters,
-                                                    vector_parameters,
-                                                    natural_parameters),
-                             t_span=[min(times), max(times)],
-                             y0=initial_states,
-                             t_eval=times)
-
-    # plt.plot(SIR_solution.t, SIR_solution.y[0], label="Na", linestyle="-")
-    # plt.plot(SIR_solution.t, SIR_solution.y[1], label="Nb", linestyle="-")
-    # plt.plot(SIR_solution.t, SIR_solution.y[2], label="V", linestyle="-")
-    plt.plot(SIR_solution.t[:2000], SIR_solution.y[3][:2000], label="Ya1", linestyle="-", alpha=0.5)
-    plt.plot(SIR_solution.t[:2000], SIR_solution.y[4][:2000], label="Ya2", linestyle="-", alpha=0.5)
-    # plt.plot(SIR_solution.t, SIR_solution.y[5], label="Ya12", linestyle="-")
-    # plt.plot(SIR_solution.t, SIR_solution.y[6], label="Yb1", linestyle="-")
-    # plt.plot(SIR_solution.t, SIR_solution.y[7], label="Yb2", linestyle="-")
-    # plt.plot(SIR_solution.t, SIR_solution.y[8], label="Yb12", linestyle="-")
-    # plt.plot(SIR_solution.t, SIR_solution.y[9], label="X1", linestyle="-")
-    # plt.plot(SIR_solution.t, SIR_solution.y[9], label="X2", linestyle="-")
-    # plt.plot(SIR_solution.t, SIR_solution.y[9], label="X12", linestyle="-")
-    plt.legend()
-    plt.show()
-
-if __name__ == "__main__":
-    main()
